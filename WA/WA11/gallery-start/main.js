a6afcb526f8089cfbe8cfa0bffa1464d612a1a13
katie-gibson <katielizgibson@gmail.com>
2023-11-12 22:34:56 -0700

    wa11

---
+++ WA/WA11/gallery-start/main.js
@@ -0,0 +1,42 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['1.jpeg',`2.jpeg`,`3.jpeg`,`4.jpeg`,`5.jpeg`];
+/* Declaring the alternative text for each image file */
+const alts = {
+    '1.jpeg' : 'picture 1',
+    '2.jpeg' : 'picture 2',
+    '3.jpeg' : 'picture 3',
+    '4.jpeg' : 'picture 4',
+    '5.jpeg' : 'picture 5'
+}
+
+/* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
+
+/* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
