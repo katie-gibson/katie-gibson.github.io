b0554129d4c82f61f7fc4a0fef0f3c3b3494f65a
katie-gibson <katielizgibson@gmail.com>
2023-11-15 15:36:08 -0700

    ica12 pt2

--- ICA/ICA12/ica12.js
+++ ICA/ICA12/ica12.js
@@ -1,41 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const newQuoteButton = document.getElementById('js-new-quote');
-    const answerButton = document.getElementById('js-answer-link');
-  
-    newQuoteButton.addEventListener('click', getQuote);
-  
-    answerButton.addEventListener('click', showAnswer);
-  
-    function getQuote() {
-      console.log('Button clicked!');
-  
-      const apiUrl = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
-  
-      fetch(apiUrl)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log('Fetched quote:', data.question);
-          displayQuote(data.question);
-        })
-        .catch(error => {
-          console.error('Error fetching quote:', error);
-          alert('Error fetching quote. Please try again.');
-        });
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const answerBtn = document.querySelector('#js-new-answer').addEventListener('click', displayAnswer);
+
+const questionTxt = document.querySelector('#js-quote-text');
+let answerTxt = document.querySelector('#js-answer-text');
+
+let answer = '';
+
+// this is the endpoint for the API that we want to get a reponse from
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote() {
+   // try -> tries something; if it returns an error, it puts us into the catch block
+    try {
+        const response = await fetch(endpoint);
+        // if !response.ok is "if the response ISN'T okay (status code 200)"
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        
+        // JSON is a dictionary, which is like a list; we call specific pieces of information out based on the 'key' associated with that value
+        displayQuote(json['question']);
+        answer = json['answer'];
+        answerTxt.textContent = '';
     }
-  
-    function displayQuote(quote) {
-      const quoteTextElement = document.getElementById('js-quote-text');
-      quoteTextElement.textContent = quote;
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch new quote');
     }
-  
-    function showAnswer() {
-      const answerTextElement = document.getElementById('js-answer-text');
-      answerTextElement.textContent = `${data.answer}`;
-    }
-  });
+}
+
+// this function shows the question
+function displayQuote(question) {
+    questionTxt.textContent = question;
+}
+
+// this function shows the answer
+function displayAnswer() {
+    answerTxt.textContent = answer;
+}
+
+// we run getQuote once when the script first loads to populate a question
+// when the page is loading
+getQuote();
   
